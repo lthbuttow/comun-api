07d75d626c76514a8af853a2b1004aadd0512957
lucasbuttow <lucas_buttow@outlook.com>
2020-02-23 18:05:46 -0300

    adjusting routes

--- src/routes.js
+++ src/routes.js
@@ -7,11 +7,11 @@ import UserController from './app/controllers/UserController';
 const routes = new Router();
 
 routes.post('/auth', AuthController.store);
-
-routes.get('/user', UserController.index);
 routes.post('/user', UserController.store);
 
 routes.use(authMiddleware);
+
+routes.get('/user', UserController.index);
 routes.get('/user/:id', UserController.show);
 routes.put('/user/:id', UserController.update);
 routes.delete('/user/:id', UserController.delete);
