061bbb4c6c9cef4e989f83a87abf55fc7991d44a
lucasbuttow <lucas_buttow@outlook.com>
2020-02-26 09:49:08 -0300

    adjusting env variables and build scripts

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import authConfig from '../../config/auth.json';
+import authConfig from '../../config/auth';
 
 // eslint-disable-next-line consistent-return
 export default (req, res, next) => {
